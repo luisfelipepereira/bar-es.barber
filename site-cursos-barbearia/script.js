c082bdd67f2ee50e61e8cd45ee356ebc9d5b56a7
Luiz Felipe Pereira de lima <luisfelipepereira508@gmail.com>
2026-02-26 02:26:56 -0300

    site modificado

--- site-cursos-barbearia/script.js
+++ site-cursos-barbearia/script.js
@@ -1,23 +1,36 @@
 const menuMobile = document.querySelector(".menu-mobile");
 const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
 
 menuMobile.addEventListener("click", () => {
   menu.classList.toggle("ativo");
+  document.body.classList.toggle("no-scroll");
+
+  // Impede header de esconder quando menu estiver aberto
+  header.classList.remove("hide");
 });
 
-const imagens = document.querySelectorAll(".img-galeria");
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modal-img");
+let lastScroll = 0;
 
-imagens.forEach(img => {
-  img.addEventListener("click", () => {
-    modal.style.display = "flex";
-    modalImg.src = img.src;
-  });
-});
+window.addEventListener("scroll", () => {
 
-modal.addEventListener("click", () => {
-  modal.style.display = "none";
+  // Se menu estiver aberto, não executar efeito
+  if (menu.classList.contains("ativo")) return;
+
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    header.classList.remove("hide");
+    return;
+  }
+
+  if (currentScroll > lastScroll && !header.classList.contains("hide")) {
+    header.classList.add("hide");
+  } else if (currentScroll < lastScroll) {
+    header.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
 });
 
 // ANIMAÇÃO AO SCROLL PREMIUM
@@ -46,4 +59,36 @@ window.addEventListener("load", revealOnScroll);
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("scrolled", window.scrollY > 50);
+});
+// HEADER MUDA AO ROLAR
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+document.querySelectorAll(".menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("ativo");
+    document.body.classList.remove("no-scroll");
+  });
+});
+
+
+// ANIMAÇÃO LATERAL COM INTERSECTION OBSERVER
+
+const elements = document.querySelectorAll(".reveal-left, .reveal-right");
+
+const observerSide = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+      observerSide.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elements.forEach(el => {
+  observerSide.observe(el);
 });
